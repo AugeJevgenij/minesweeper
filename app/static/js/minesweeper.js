918acb5821c0d2224ffe758b02d4720b09a6fea8
AugeJevgenij <augejevgenij@gmail.com>
2022-10-04 16:31:29 +0300

    gameboard

--- app/static/js/minesweeper.js
+++ app/static/js/minesweeper.js
@@ -1,6 +1,23 @@
+window.addEventListener('load', function () {
+    const gameboard = document.getElementById("gameboard");
+    const url = "/minesweeper/new_game";
 
-const url = "/minesweeper/new_game";
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => createGameboard(data));
 
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
+    function createGameboard(data) {
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j++) {
+                // console.log(i, j);
+                const newSquare = document.createElement("div");
+                newSquare.classList.add("cell");
+                newSquare.setAttribute("id", `${i}_${j}`);
+                gameboard.appendChild(newSquare);
+            }
+        }
+    }
+
+    gameboard.addEventListener('click', (e) => {console.log(e.target)});
+})
